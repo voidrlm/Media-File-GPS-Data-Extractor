4b7c4047cc828672b7a3a09de65f443081ea21e8
voidrlm <varjun609@gmail.com>
2023-10-07 15:59:24 +0530

    Added error handling for image and video extraction

--- ImageAndVideo.py
+++ ImageAndVideo.py
@@ -1,7 +1,12 @@
 import exiftool
-with exiftool.ExifToolHelper() as et:
-    # Provide file path here (Can use both image and video files)
-    filepath = "./media/image.jpg"
-    gps_data = et.get_metadata(
-        filepath, ['-gpslatitude', '-gpslongitude'])
-    print(gps_data)
+try:
+    with exiftool.ExifToolHelper() as et:
+        # Provide file path here (Can use both image and video files)
+        filepath = "./media/image.jpg"
+        gps_data = et.get_metadata(
+            filepath, ['-gpslatitude', '-gpslongitude'])
+        print(gps_data)
+except exiftool.ExifToolError as e:
+    print(f"ExifToolError: {e}")
+except Exception as e:
+    print(f"An error occurred: {e}")
