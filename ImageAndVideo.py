c16e2425298cdd717e79c17f2d323f94c4fc7dae
voidrlm <varjun609@gmail.com>
2023-10-07 16:05:37 +0530

    Code improvements for image and video geodata extraction

--- ImageAndVideo.py
+++ ImageAndVideo.py
@@ -1,12 +1,34 @@
 import exiftool
-try:
-    with exiftool.ExifToolHelper() as et:
-        # Provide file path here (Can use both image and video files)
-        filepath = "./media/image.jpg"
-        gps_data = et.get_metadata(
-            filepath, ['-gpslatitude', '-gpslongitude'])
+
+
+def get_gps_data(filepath):
+    try:
+        with exiftool.ExifToolHelper() as et:
+            # Get GPS latitude and longitude metadata
+            gps_data = et.get_metadata(
+                filepath, ['-gpslatitude', '-gpslongitude'])
+            return gps_data
+    except exiftool.ExifToolError as e:
+        return f"ExifToolError: {e}"
+    except FileNotFoundError:
+        return f"File not found: {filepath}"
+    except Exception as e:
+        return f"An unexpected error occurred: {e}"
+
+
+def main():
+    # Provide file path here (Can use both image and video files)
+    filepath = "./media/image.jpg"
+
+    # Get GPS data and handle errors
+    gps_data = get_gps_data(filepath)
+
+    if isinstance(gps_data, str):
+        print(f"Error: {gps_data}")
+    else:
+        print("GPS Data:")
         print(gps_data)
-except exiftool.ExifToolError as e:
-    print(f"ExifToolError: {e}")
-except Exception as e:
-    print(f"An error occurred: {e}")
+
+
+if __name__ == "__main__":
+    main()
